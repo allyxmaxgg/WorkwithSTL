8faf015e89ee7db3c3051fa3eff2d802b7e1586e
acer <acer@DESKTOP-0LETJUQ>
2025-10-07 18:29:15 +0300

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+
+int main() {
+    std::list<int> numbers;
+
+    numbers.push_back(10);
+    numbers.push_back(20);
+    numbers.push_back(30);
+
+    numbers.pop_front();
+    numbers.pop_back();
+
+    for (int x : numbers) {
+        std::cout << x << " ";
+    }
+}
